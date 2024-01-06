3d68b7cddc268c1c0d9dfca6bc93eedfeb871669
Marycrazy <marianapaisjorgemagalhaes@gmail.com>
2024-01-06 23:51:46 +0000

    organizaçao

--- codigo para o relatorio/R[sql4].sql
+++ codigo para o relatorio/R[sql4].sql
@@ -3,20 +3,13 @@ precisa ter uma maneira de identificar a categoria de um produto. Suponha que vo
 coluna categoria na tabela verificaçao_venda que identifica a categoria de cada produto.
 Vamos supor que você queira encontrar produtos alternativos da mesma categoria que um produto 
 específico chamado 'produto1'. Você pode usar o seguinte comando SQL:*/
-SELECT 
- veri_Produto AS Produto_Alternativo
-FROM 
- verificaçao_venda
-WHERE 
- veri_Produto != 'produto1' AND
- categoria = (
-   SELECT 
-     categoria 
-   FROM 
-     verificaçao_venda 
-   WHERE 
-     veri_Produto = 'produto1'
- );
+SELECT veri_Produto AS Produto_Alternativo
+
+FROM verificaçao_venda
+
+WHERE veri_Produto != 'produto1' AND
+ categoria = (SELECT categoria FROM verificaçao_venda WHERE veri_Produto = 'produto1');
+ 
 /*Neste comando, a subconsulta (SELECT categoria FROM verificaçao_venda WHERE veri_Produto = 'produto1') retorna 
 a categoria do 'produto1'. A consulta principal então seleciona todos os produtos que não são 'produto1' e 
 estão na mesma categoria.
